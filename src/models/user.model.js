0f217f1fb349ad4d4841345f224716c699625ce6
IslamMostafaYassin <islam.mostafa.yassin@gmail.com>
2026-07-26 21:56:39 +0300

    feat(user-model): add role field

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,28 @@
 const mongoose=require("mongoose")
 
 const userSchema=new mongoose.Schema({
-	username:{
-		type:String,
-		required:true,
-		unique:true
+		username:{
+			type:String,
+			required:true,
+			unique:true
+		},
+		email:{
+			type:String,
+			required:true,
+			unique:true
+		},
+		hashedPassword:{
+			type:String,
+			required:true,
+		},
+		role:{
+			type:String,
+			enum:["user","admin"],
+			default:"user"
+		}
 	},
-	email:{
-		type:String,
-		required:true,
-		unique:true
-	},
-	hashedPassword:{
-		type:String,
-		required:true,
-	},
+{
+	timestamps:true
 })
 
 const userModel=mongoose.model("User",userSchema)
